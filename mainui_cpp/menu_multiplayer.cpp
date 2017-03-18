06e0d669414168c2809fc53e450bcc3b83851d14
Alibek Omarov (a1batross) <a1ba.omarov@gmail.com>
2017-03-19 01:35:55 +0300

    Port multiplayer menu

--- mainui_cpp/menu_multiplayer.cpp
+++ mainui_cpp/menu_multiplayer.cpp
@@ -20,81 +20,55 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "extdll.h"
 #include "basemenu.h"
+#include "Bitmap.h"
+#include "YesNoMessageBox.h"
 #include "utils.h"
 #include "menu_btnsbmp_table.h"
 #include "keydefs.h"
 
 #define ART_BANNER			"gfx/shell/head_multi"
 
-#define ID_BACKGROUND		0
-#define ID_BANNER			1
-
-#define ID_INTERNETGAMES		2
-#define ID_SPECTATEGAMES		3
-#define ID_LANGAME			4
-#define ID_CUSTOMIZE		5
-#define ID_CONTROLS			6
-#define ID_DONE			7
-
-#define ID_MSGBOX	 	8
-#define ID_MSGTEXT	 	9
-#define ID_YES		130
-#define ID_NO	 	131
-
-typedef struct
+class CMenuMultiplayer : public CMenuFramework
 {
-	menuFramework_s	menu;
+public:
+	virtual const char *Key(int key, int down);
 
-	menuBitmap_s	background;
-	menuBitmap_s	banner;
+private:
+	virtual void _Init();
+	virtual void _VidInit();
+	static void PredictDialog( CMenuBaseItem *pSelf, void *pExtra );
 
-	menuPicButton_s	internetGames;
-	menuPicButton_s	spectateGames;
-	menuPicButton_s	LANGame;
-	menuPicButton_s	Customize;	// playersetup
-	menuPicButton_s	Controls;
-	menuPicButton_s	done;
+	CMenuBackgroundBitmap	background;
+	CMenuBannerBitmap		banner;
+
+	CMenuPicButton	internetGames;
+	CMenuPicButton	spectateGames;
+	CMenuPicButton	LANGame;
+	CMenuPicButton	Customize;	// playersetup
+	CMenuPicButton	Controls;
+	CMenuPicButton	done;
 
 	// prompt dialog
-	menuAction_s	msgBox;
-	menuAction_s	promptMessage;
-	menuAction_s	promptMessage2;
-	menuAction_s	promptMessage3;
-	menuPicButton_s	yes;
-	menuPicButton_s	no;
-} uiMultiPlayer_t;
+	CMenuYesNoMessageBox msgBox;
+};
 
-static uiMultiPlayer_t	uiMultiPlayer;
+static CMenuMultiplayer	uiMultiPlayer;
 
-/*
-=================
-UI_MsgBox_Ownerdraw
-=================
-*/
-static void UI_MsgBox_Ownerdraw( void *self )
+
+void CMenuMultiplayer::PredictDialog( CMenuBaseItem *pSelf, void *pExtra )
 {
-	menuCommon_s	*item = (menuCommon_s *)self;
+	CMenuMultiplayer *parent = (CMenuMultiplayer*)pSelf->Parent();
 
-	UI_FillRect( item->x, item->y, item->width, item->height, uiPromptBgColor );
-}
-
-static void UI_PredictDialog( void )
-{
 	// toggle main menu between active\inactive
 	// show\hide remove dialog
-	uiMultiPlayer.internetGames.generic.flags ^= QMF_INACTIVE;
-	uiMultiPlayer.spectateGames.generic.flags ^= QMF_INACTIVE;
-	uiMultiPlayer.LANGame.generic.flags ^= QMF_INACTIVE;
-	uiMultiPlayer.Customize.generic.flags ^= QMF_INACTIVE;
-	uiMultiPlayer.Controls.generic.flags ^= QMF_INACTIVE;
-	uiMultiPlayer.done.generic.flags ^= QMF_INACTIVE;
+	parent->internetGames.Flags() ^= QMF_INACTIVE;
+	parent->spectateGames.Flags() ^= QMF_INACTIVE;
+	parent->LANGame.Flags() ^= QMF_INACTIVE;
+	parent->Customize.Flags() ^= QMF_INACTIVE;
+	parent->Controls.Flags() ^= QMF_INACTIVE;
+	parent->done.Flags() ^= QMF_INACTIVE;
 
-	uiMultiPlayer.msgBox.generic.flags ^= QMF_HIDDEN;
-	uiMultiPlayer.promptMessage.generic.flags ^= QMF_HIDDEN;
-	uiMultiPlayer.promptMessage2.generic.flags ^= QMF_HIDDEN;
-	uiMultiPlayer.promptMessage3.generic.flags ^= QMF_HIDDEN;
-	uiMultiPlayer.no.generic.flags ^= QMF_HIDDEN;
-	uiMultiPlayer.yes.generic.flags ^= QMF_HIDDEN;
+	parent->msgBox.ToggleVisibility();
 }
 
 
@@ -103,231 +77,112 @@ static void UI_PredictDialog( void )
 UI_Multiplayer_KeyFunc
 =================
 */
-static const char *UI_Multiplayer_KeyFunc( int key, int down )
+const char *CMenuMultiplayer::Key( int key, int down )
 {
-	if( down && key == K_ESCAPE && !( uiMultiPlayer.promptMessage.generic.flags & QMF_HIDDEN ))
+	if( down && key == K_ESCAPE && !( msgBox.Flags() & QMF_HIDDEN ))
 	{
-		UI_PredictDialog();
+		PredictDialog( &msgBox, NULL );
 		return uiSoundNull;
 	}
-	return UI_DefaultKey( &uiMultiPlayer.menu, key, down );
+	return CMenuFramework::Key( key, down );
 }
 
 
 /*
 =================
-UI_MultiPlayer_Callback
+CMenuMultiplayer::Init
 =================
 */
-static void UI_MultiPlayer_Callback( void *self, int event )
+void CMenuMultiplayer::_Init( void )
 {
-	menuCommon_s	*item = (menuCommon_s *)self;
+	// memset( &uiMultiPlayer, 0, sizeof( CMenuMultiplayer ));
 
-	if( event != QM_ACTIVATED )
-		return;
+	banner.SetPicture( ART_BANNER );
 
-	switch( item->id )
+	internetGames.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
+	internetGames.SetNameAndStatus( "Internet game", "View list of a game internet servers and join the one of your choice" );
+	internetGames.SetPicture( PC_INET_GAME );
+	// SET_EVENT_VOID( internetGames, onActivated, UI_InternetGames_Menu );
+
+	spectateGames.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY|QMF_GRAYED;
+	spectateGames.SetNameAndStatus( "Spectate game", "Spectate internet games" );
+	spectateGames.SetPicture( PC_SPECTATE_GAMES );
+	// TODO: implement
+	// SET_EVENT_VOID( spectateGames, onActivated, UI_SpectateGames_Menu );
+
+	LANGame.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
+	LANGame.SetNameAndStatus( "LAN game", "Set up the game on the local area network" );
+	LANGame.SetPicture( PC_LAN_GAME );
+	// SET_EVENT_VOID( LANGame, onActivated, UI_LANGames_Menu );
+
+	Customize.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
+	Customize.SetNameAndStatus( "Customize", "Choose your player name, and select visual options for your character" );
+	Customize.SetPicture( PC_CUSTOMIZE );
+	// SET_EVENT_VOID( Customize, onActivated, UI_PlayerSetup_Menu );
+
+	Controls.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
+	Controls.SetNameAndStatus( "Controls", "Change keyboard and mouse settings" );
+	Controls.SetPicture( PC_CONTROLS );
+	// SET_EVENT_VOID( Controls, onActivated, UI_Controls_Menu );
+
+	done.Flags() = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
+	done.SetNameAndStatus( "Done", "Go back to the Main menu" );
+	done.SetPicture( PC_DONE );
+	done.onActivated = PopMenuCb;
+
+	msgBox.SetMessage( "It is recomended to enable\nclient movement prediction\nPress OK to enable it now\nOr enable it later in\n^5(Multiplayer/Customize)");
+	msgBox.SetPositiveButton( "Ok", PC_OK );
+	msgBox.SetNegativeButton( "Cancel", PC_CANCEL );
+	msgBox.HighlightChoice( true );
+	SET_EVENT( msgBox, onPositive )
 	{
-	case ID_INTERNETGAMES:
-		UI_InternetGames_Menu();
-		break;
-	case ID_SPECTATEGAMES:
-		// UNDONE: not implemented
-		break;
-	case ID_LANGAME:
-		UI_LanGame_Menu();
-		break;
-	case ID_CUSTOMIZE:
-		UI_PlayerSetup_Menu();
-		break;
-	case ID_CONTROLS:
-		UI_Controls_Menu();
-		break;
-	case ID_DONE:
-		UI_PopMenu();
-		break;
-	case ID_YES:
-		CVAR_SET_FLOAT( "cl_predict", 1 );
-	case ID_NO:
-		CVAR_SET_FLOAT( "menu_mp_firsttime", 0 );
-		UI_PredictDialog();
-		break;
+		EngFuncs::CvarSetValue( "cl_predict", 1.0f );
+		EngFuncs::CvarSetValue( "menu_mp_firsttime", 0.0f );
+		CMenuMultiplayer::PredictDialog( pSelf, pExtra );
 	}
+	END_EVENT( msgBox, onPositive )
+
+	SET_EVENT( msgBox, onNegative )
+	{
+		EngFuncs::CvarSetValue( "menu_mp_firsttime", 0.0f );
+		CMenuMultiplayer::PredictDialog( pSelf, pExtra );
+	}
+	END_EVENT( msgBox, onNegative )
+
+	AddItem( background );
+	AddItem( banner );
+	AddItem( internetGames );
+	AddItem( spectateGames );
+	AddItem( LANGame );
+	AddItem( Customize );
+	AddItem( Controls );
+	AddItem( done );
+	AddItem( msgBox );
+
+	//if( EngFuncs::GetCvarFloat( "menu_mp_firsttime" ) && !EngFuncs::GetCvarFloat( "cl_predict" ) )
+	if( 1 )
+		PredictDialog( &msgBox, NULL );
 }
 
-/*
-=================
-UI_MultiPlayer_Init
-=================
-*/
-static void UI_MultiPlayer_Init( void )
+void CMenuMultiplayer::_VidInit()
 {
-	// memset( &uiMultiPlayer, 0, sizeof( uiMultiPlayer_t ));
-	uiMultiPlayer.menu.keyFunc = UI_Multiplayer_KeyFunc;
-
-	uiMultiPlayer.menu.vidInitFunc = UI_MultiPlayer_Init;
-
-	uiMultiPlayer.background.generic.id = ID_BACKGROUND;
-	uiMultiPlayer.background.generic.type = QMTYPE_BITMAP;
-	uiMultiPlayer.background.generic.flags = QMF_INACTIVE;
-	uiMultiPlayer.background.generic.x = 0;
-	uiMultiPlayer.background.generic.y = 0;
-	uiMultiPlayer.background.generic.width = uiStatic.width;
-	uiMultiPlayer.background.generic.height = 768;
-	uiMultiPlayer.background.pic = ART_BACKGROUND;
-
-	uiMultiPlayer.banner.generic.id = ID_BANNER;
-	uiMultiPlayer.banner.generic.type = QMTYPE_BITMAP;
-	uiMultiPlayer.banner.generic.flags = QMF_INACTIVE|QMF_DRAW_ADDITIVE;
-	uiMultiPlayer.banner.generic.x = UI_BANNER_POSX;
-	uiMultiPlayer.banner.generic.y = UI_BANNER_POSY;
-	uiMultiPlayer.banner.generic.width = UI_BANNER_WIDTH;
-	uiMultiPlayer.banner.generic.height = UI_BANNER_HEIGHT;
-	uiMultiPlayer.banner.pic = ART_BANNER;
-
-	uiMultiPlayer.internetGames.generic.id = ID_INTERNETGAMES;
-	uiMultiPlayer.internetGames.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.internetGames.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
-	uiMultiPlayer.internetGames.generic.x = 72;
-	uiMultiPlayer.internetGames.generic.y = 230;
-	uiMultiPlayer.internetGames.generic.name = "Internet games";
-	uiMultiPlayer.internetGames.generic.statusText = "View list of a game internet servers and join the one of your choise";
-	uiMultiPlayer.internetGames.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.internetGames, PC_INET_GAME );
-
-	uiMultiPlayer.spectateGames.generic.id = ID_SPECTATEGAMES;
-	uiMultiPlayer.spectateGames.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.spectateGames.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY|QMF_GRAYED;
-	uiMultiPlayer.spectateGames.generic.x = 72;
-	uiMultiPlayer.spectateGames.generic.y = 280;
-	uiMultiPlayer.spectateGames.generic.name = "Spectate games";
-	uiMultiPlayer.spectateGames.generic.statusText = "Spectate internet games";
-	uiMultiPlayer.spectateGames.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.spectateGames, PC_SPECTATE_GAMES );
-
-	uiMultiPlayer.LANGame.generic.id = ID_LANGAME;
-	uiMultiPlayer.LANGame.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.LANGame.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
-	uiMultiPlayer.LANGame.generic.x = 72;
-	uiMultiPlayer.LANGame.generic.y = 330;
-	uiMultiPlayer.LANGame.generic.name = "LAN game";
-	uiMultiPlayer.LANGame.generic.statusText = "Set up the game on the local area network";
-	uiMultiPlayer.LANGame.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.LANGame, PC_LAN_GAME );
-
-	uiMultiPlayer.Customize.generic.id = ID_CUSTOMIZE;
-	uiMultiPlayer.Customize.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.Customize.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
-	uiMultiPlayer.Customize.generic.x = 72;
-	uiMultiPlayer.Customize.generic.y = 380;
-	uiMultiPlayer.Customize.generic.name = "Customize";
-	uiMultiPlayer.Customize.generic.statusText = "Choose your player name, and select visual options for your character";
-	uiMultiPlayer.Customize.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.Customize, PC_CUSTOMIZE );
-
-	uiMultiPlayer.Controls.generic.id = ID_CONTROLS;
-	uiMultiPlayer.Controls.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.Controls.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
-	uiMultiPlayer.Controls.generic.x = 72;
-	uiMultiPlayer.Controls.generic.y = 430;
-	uiMultiPlayer.Controls.generic.name = "Controls";
-	uiMultiPlayer.Controls.generic.statusText = "Change keyboard and mouse settings";
-	uiMultiPlayer.Controls.generic.callback = UI_MultiPlayer_Callback;
-	
-	UI_UtilSetupPicButton( &uiMultiPlayer.Controls, PC_CONTROLS );
-
-	uiMultiPlayer.done.generic.id = ID_DONE;
-	uiMultiPlayer.done.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.done.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_NOTIFY;
-	uiMultiPlayer.done.generic.x = 72;
-	uiMultiPlayer.done.generic.y = 480;
-	uiMultiPlayer.done.generic.name = "Done";
-	uiMultiPlayer.done.generic.statusText = "Go back to the Main Menu";
-	uiMultiPlayer.done.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.done, PC_DONE );
-
-	uiMultiPlayer.msgBox.generic.id = ID_MSGBOX;
-	uiMultiPlayer.msgBox.generic.type = QMTYPE_ACTION;
-	uiMultiPlayer.msgBox.generic.flags = QMF_INACTIVE|QMF_HIDDEN;
-	uiMultiPlayer.msgBox.generic.ownerdraw = UI_MsgBox_Ownerdraw; // just a fill rectangle
-	uiMultiPlayer.msgBox.generic.x = DLG_X + 192;
-	uiMultiPlayer.msgBox.generic.y = 256;
-	uiMultiPlayer.msgBox.generic.width = 640;
-	uiMultiPlayer.msgBox.generic.height = 256;
-
-	uiMultiPlayer.promptMessage.generic.id = ID_MSGBOX;
-	uiMultiPlayer.promptMessage.generic.type = QMTYPE_ACTION;
-	uiMultiPlayer.promptMessage.generic.flags = QMF_INACTIVE|QMF_DROPSHADOW|QMF_HIDDEN;
-	uiMultiPlayer.promptMessage.generic.name = "It is recomended to enable\nclient movement prediction";
-	uiMultiPlayer.promptMessage.generic.x = DLG_X + 270;
-	uiMultiPlayer.promptMessage.generic.y = 280;
-
-	uiMultiPlayer.promptMessage2.generic.id = ID_MSGBOX;
-	uiMultiPlayer.promptMessage2.generic.type = QMTYPE_ACTION;
-	uiMultiPlayer.promptMessage2.generic.flags = QMF_INACTIVE|QMF_DROPSHADOW|QMF_HIDDEN;
-	uiMultiPlayer.promptMessage2.generic.name = " Or enable it later in\n^5(Multiplayer/Customize)";
-	uiMultiPlayer.promptMessage2.generic.x = DLG_X + 310;
-	uiMultiPlayer.promptMessage2.generic.y = 340;
-
-	uiMultiPlayer.promptMessage3.generic.id = ID_MSGBOX;
-	uiMultiPlayer.promptMessage3.generic.type = QMTYPE_ACTION;
-	uiMultiPlayer.promptMessage3.generic.flags = QMF_INACTIVE|QMF_DROPSHADOW|QMF_HIDDEN;
-	uiMultiPlayer.promptMessage3.generic.name = "Press OK to enable it now";
-	uiMultiPlayer.promptMessage3.generic.x = DLG_X + 290;
-	uiMultiPlayer.promptMessage3.generic.y = 400;
-
-	uiMultiPlayer.yes.generic.id = ID_YES;
-	uiMultiPlayer.yes.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.yes.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_HIDDEN;
-	uiMultiPlayer.yes.generic.name = "Ok";
-	uiMultiPlayer.yes.generic.x = DLG_X + 380;
-	uiMultiPlayer.yes.generic.y = 460;
-	uiMultiPlayer.yes.generic.callback = UI_MultiPlayer_Callback;
-
-	UI_UtilSetupPicButton( &uiMultiPlayer.yes, PC_OK );
-
-	uiMultiPlayer.no.generic.id = ID_NO;
-	uiMultiPlayer.no.generic.type = QMTYPE_BM_BUTTON;
-	uiMultiPlayer.no.generic.flags = QMF_HIGHLIGHTIFFOCUS|QMF_DROPSHADOW|QMF_HIDDEN;
-	uiMultiPlayer.no.generic.name = "Cancel";
-	uiMultiPlayer.no.generic.x = DLG_X + 530;
-	uiMultiPlayer.no.generic.y = 460;
-	uiMultiPlayer.no.generic.callback = UI_MultiPlayer_Callback;
-	UI_UtilSetupPicButton( &uiMultiPlayer.no, PC_CANCEL );
-
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.background );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.banner );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.internetGames );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.spectateGames );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.LANGame );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.Customize );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.Controls );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.done );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.msgBox );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.promptMessage );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.promptMessage2 );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.promptMessage3 );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.no );
-	UI_AddItem( &uiMultiPlayer.menu, (void *)&uiMultiPlayer.yes );
-
-	if( CVAR_GET_FLOAT( "menu_mp_firsttime" ) && !CVAR_GET_FLOAT( "cl_predict" ) )
-		UI_PredictDialog();
+	internetGames.SetCoord( 72, 230 );
+	spectateGames.SetCoord( 72, 280 );
+	LANGame.SetCoord( 72, 330 );
+	Customize.SetCoord( 72, 380 );
+	Controls.SetCoord( 72, 430 );
+	done.SetCoord( 72, 480 );
 }
 
 /*
 =================
-UI_MultiPlayer_Precache
+CMenuMultiplayer::Precache
 =================
 */
 void UI_MultiPlayer_Precache( void )
 {
-	PIC_Load( ART_BACKGROUND );
-	PIC_Load( ART_BANNER );
+	EngFuncs::PIC_Load( ART_BACKGROUND );
+	EngFuncs::PIC_Load( ART_BANNER );
 }
 
 /*
@@ -341,7 +196,8 @@ void UI_MultiPlayer_Menu( void )
 		return;
 
 	UI_MultiPlayer_Precache();
-	UI_MultiPlayer_Init();
+	uiMultiPlayer.Init();
+	uiMultiPlayer.VidInit();
 
-	UI_PushMenu( &uiMultiPlayer.menu );
+	uiMultiPlayer.PushMenu();
 }
